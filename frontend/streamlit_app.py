c6180210b8a8576e937a7d0aad3fc9437d48d1e3
Tanya <tanyaagrawal256@gmail.com>
2026-02-11 11:16:44 +0530

    Connected frontend to deploy backend

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # FastAPI endpoint
-API_URL = ""
+API_URL = "https://cicd-pipeline-eup2.onrender.com/predict"
 
 st.set_page_config(
     page_title="House Price Prediction",
